5694e2a719260a966e678fc112b376f77a8b47d0
samyak-05 <167967864+samyak-05@users.noreply.github.com>
2025-06-09 23:00:26 +0530

    Add files via upload

---
+++ googleform.sql
@@ -0,0 +1,30 @@
+create database google;
+
+use google;
+
+create table admins(
+aname varchar(40),
+apass varchar(20)
+);
+
+create table info(
+
+user_name varchar(25),
+phone_no bigint,
+email varchar(50),
+city varchar(20),
+state varchar(30),
+pincode int,
+college varchar(70)
+);
+
+
+
+
+
+
+
+
+
+
+
